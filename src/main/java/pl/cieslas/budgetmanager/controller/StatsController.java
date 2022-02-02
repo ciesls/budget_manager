c07f45fab892583d5e81620ac860288c3c1a6450
cieslas <70605978+ciesls@users.noreply.github.com>
2022-02-02 14:16:09 +0100

    Commit

--- src/main/java/pl/cieslas/budgetmanager/controller/StatsController.java
+++ src/main/java/pl/cieslas/budgetmanager/controller/StatsController.java
@@ -1,46 +1,46 @@
-//package pl.cieslas.budgetmanager.controller;
-//
-//import org.springframework.format.annotation.DateTimeFormat;
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import pl.cieslas.budgetmanager.entity.Category;
-//import pl.cieslas.budgetmanager.entity.Expense;
-//import pl.cieslas.budgetmanager.repository.category.CategoryService;
-//import pl.cieslas.budgetmanager.repository.expense.ExpenseService;
-//import pl.cieslas.budgetmanager.security.CurrentUser;
-//import pl.cieslas.budgetmanager.utils.ExpenseUtils.ExpenseUtils;
-//
-//import java.time.LocalDate;
-//import java.util.List;
-//
-//@RequestMapping("/stats")
-//@Controller
-//public class StatsController {
-//
-//    private final CategoryService categoryService;
-//    private final ExpenseUtils expenseUtils;
-//    private final ExpenseService expenseService;
-//
-//    public StatsController(CategoryService categoryService, ExpenseUtils expenseUtils, ExpenseService expenseService) {
-//        this.categoryService = categoryService;
-//        this.expenseUtils = expenseUtils;
-//        this.expenseService = expenseService;
-//    }
-//
-//    @PostMapping("/categoriesMonth")
-//    public String categoryStats(@RequestParam("startDate") @DateTimeFormat(pattern = "dd/mm/yyyy") LocalDate startDate,
-//                                @RequestParam("endTime") @DateTimeFormat(pattern = "dd/mm/YYYY") LocalDate endDate,
-//                                @AuthenticationPrincipal CurrentUser currentUser,
-//                                @RequestParam Category category) {
-//
-////        List<Expense> expenses = expenseService.findAllByCategoryAndUserAndCreatedOnBetween(category, currentUser.getUser(), startDate, endDate);
-//
-//
-//        return "";
-//    }
-//
-//
-//}
+package pl.cieslas.budgetmanager.controller;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.cieslas.budgetmanager.entity.Category;
+import pl.cieslas.budgetmanager.entity.Expense;
+import pl.cieslas.budgetmanager.repository.category.CategoryService;
+import pl.cieslas.budgetmanager.repository.expense.ExpenseService;
+import pl.cieslas.budgetmanager.security.CurrentUser;
+import pl.cieslas.budgetmanager.utils.ExpenseUtils.ExpenseUtils;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RequestMapping("/stats")
+@Controller
+public class StatsController {
+
+    private final CategoryService categoryService;
+    private final ExpenseUtils expenseUtils;
+    private final ExpenseService expenseService;
+
+    public StatsController(CategoryService categoryService, ExpenseUtils expenseUtils, ExpenseService expenseService) {
+        this.categoryService = categoryService;
+        this.expenseUtils = expenseUtils;
+        this.expenseService = expenseService;
+    }
+
+    @PostMapping("/categoriesMonth")
+    public String categoryStats(@RequestParam("startDate") @DateTimeFormat(pattern = "dd/mm/yyyy") LocalDate startDate,
+                                @RequestParam("endTime") @DateTimeFormat(pattern = "dd/mm/YYYY") LocalDate endDate,
+                                @AuthenticationPrincipal CurrentUser currentUser,
+                                @RequestParam Category category) {
+
+//        List<Expense> expenses = expenseService.findAllByCategoryAndUserAndCreatedOnBetween(category, currentUser.getUser(), startDate, endDate);
+
+
+        return "";
+    }
+
+
+}
